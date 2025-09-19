5a9cd41f6d8bc579a3290572e00e31108d017cd5
Mariam-S533 <ms3514@fayoum.edu.eg>
2025-09-19 22:52:24 +0300

    update orders page url

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 
 function page() {
-  return (
-   <>
-    <div className='flex justify-center items-center h-[calc(100vh - 200px)] text-2xl font-bold'>Congrates! </div>
-   </>
-
-  )
+  return<>
+  <p className='flex justify-center items-center h-[calc(100vh-200px)] text-2xl font-bold'>Congratulate!</p>
+  </>
 }
 
 export default page

8b149d4cad043a747fa0e7bff706f827077a2746
Mariam-S533 <ms3514@fayoum.edu.eg>
2025-09-19 18:44:11 +0300

    change get token at production

--- src/lib/userToken.ts
+++ src/lib/userToken.ts
@@ -7,7 +7,9 @@ import { cookies } from "next/headers";
 
 export  async function userToken(){
 
-    const encodedToken = (await cookies()).get('next-auth.session-token')?.value
+    const cookieToken = await cookies()
+    const encodedToken = cookieToken.get('next-auth.session-token')?.value || cookieToken.get('__Secure-next-auth.session-token')?.value
+
     const token  = await decode({token: encodedToken, secret: process.env.AUTH_SECRET!})
     return token?.token 
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
 
-    const token = request.cookies.get('next-auth.session-token')
+    const token = request.cookies.get('next-auth.session-token') ||  request.cookies.get("__Secure-next-auth.session-token")
     if(!token){
             return NextResponse.redirect(new URL('/login', request.url))
 
@@ -15,6 +15,6 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     "/cart",
-    
+    "/wishlist"
   ],
 }

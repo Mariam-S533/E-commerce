c0e80ce1b627f997fe455810785a6d58e7ad910e
Mariam-S533 <ms3514@fayoum.edu.eg>
2025-09-19 19:28:01 +0300

    add base url

--- src/app/actions/payment.action.ts
+++ src/app/actions/payment.action.ts
@@ -41,7 +41,9 @@ export async function onlinePayment(cartId : string, shippingAddress: shipping) 
         throw new Error('You must be logged in to add to cart')
     }
 
-    const res = await fetch(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=http://localhost:3000`, {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+
+    const res = await fetch(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=${baseUrl}`, {
         method: 'POST',
         headers:{
             token : token as string,

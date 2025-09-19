7a19ead050a918f1338dd3d484a82333c9ff105b
Mariam-S533 <ms3514@fayoum.edu.eg>
2025-09-19 18:56:47 +0300

    update middelware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,13 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
 
-    const token = request.cookies.get('next-auth.session-token') ||  request.cookies.get("__Secure-next-auth.session-token")
+    const token = request.cookies.get('next-auth.session-token')?.value ||  request.cookies.get("__Secure-next-auth.session-token")?.value
     if(!token){
             return NextResponse.redirect(new URL('/login', request.url))
 
     }
+
+    return NextResponse.next()
 }
  
 // See "Matching Paths" below to learn more
